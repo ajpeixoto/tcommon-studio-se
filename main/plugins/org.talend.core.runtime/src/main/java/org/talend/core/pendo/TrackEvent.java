99331076a374e93b8374bbbe144e9e45d1e8fc0e
jding-tlnd <jding@talend.com>
2022-10-20 15:27:42 +0800

    fix(TUP-36862):Transfer pendo to EE https://jira.talendforge.org/browse/TUP-36862

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/pendo/TrackEvent.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/pendo/TrackEvent.java
@@ -18,16 +18,52 @@ package org.talend.core.pendo;
  */
 public enum TrackEvent {
 
+    /**
+     * create by TUP-33421 update by TUP-34791, TUP-35523, TUP-36780
+     */
     PROJECT_LOGIN("Project Login"),
+
+    /**
+     * create by TUP-33990
+     */
     IMPORT_API_DEF("Import API Definition"),
+    /**
+     * create by TUP-33990
+     */
     UPDATE_API_DEF("Update API Definition"),
+    /**
+     * create by TUP-33990
+     */
     USE_API_DEF("Use API Definition"),
+    /**
+     * create by TUP-33990
+     */
     OPEN_IN_APIDesigner("Open in API Designer"),
+    /**
+     * create by TUP-33990
+     */
     OPEN_IN_APITester("Open in API Tester"),
+    /**
+     * create by TUP-33990
+     */
     OPEN_API_DOCUMENTATION("Open API Documentation"),
+
+    /**
+     * create by TUP-35644
+     */
     AUTOMAP("tMap Automap"),
+    /**
+     * create by TUP-35644 update by TUP-36710
+     */
     TMAP("tMap"),
+
+    /**
+     * create by TUP-35712 update by TUP-36893
+     */
     ITEM_IMPORT("Import items"),
+    /**
+     * create by TUP-35712
+     */
     ITEM_SIGNATURE("Item Signature");
 
     private String event;
