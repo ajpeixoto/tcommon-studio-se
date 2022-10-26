a5fce652e27b44497cd4f233ac3d0f78ae957368
Zhiwei Xue <zwxue@talend.com>
2022-10-26 20:47:35 +0800

    feat(TUP-36683):Selective generateAllPoms (#5676)

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/runtime/services/IFilterService.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/runtime/services/IFilterService.java
@@ -1,5 +1,6 @@
 package org.talend.core.runtime.services;
 
+import org.talend.core.GlobalServiceRegister;
 import org.talend.core.IService;
 import org.talend.core.model.properties.Item;
 
@@ -8,5 +9,12 @@ public interface IFilterService extends IService {
     public String checkFilterError(String filter);
 
 	public boolean isFilterAccepted(Item item, String filter);
+
+    public static IFilterService get() {
+        if (GlobalServiceRegister.getDefault().isServiceRegistered(IFilterService.class)) {
+            return GlobalServiceRegister.getDefault().getService(IFilterService.class);
+        }
+        return null;
+    }
 
 }
