cf1b069019cb9c60c0942705839c7da8587a161a
Nicolas Rousseau <nrousseau@talend.com>
2023-01-31 14:59:17 +0800

    dddd

--- main/plugins/org.talend.librariesmanager/resources/java/routines/system/FormatterUtils.java
+++ main/plugins/org.talend.librariesmanager/resources/java/routines/system/FormatterUtils.java
@@ -32,7 +32,7 @@ public class FormatterUtils {
      */
     public static String fm(Object obj, String pattern) {
         if(obj == null) {
-        	System.out.println("test2");
+        	System.out.println("test3");
             return null;
         }
 
