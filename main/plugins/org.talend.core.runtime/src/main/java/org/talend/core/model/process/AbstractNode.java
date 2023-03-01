950c128f61decf86aa71d045251341bc314649a0
Jane Ding <jding@talend.com>
2023-03-01 19:49:37 +0800

    Revert "fix(TUP-37227):Issue building when items names too long (#5941)" (#6020)
    This reverts commit 8d3832fdfbad9a4204f4048389518f9b27203389.

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/model/process/AbstractNode.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/model/process/AbstractNode.java
@@ -18,7 +18,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-import org.apache.commons.lang.StringUtils;
 import org.talend.components.api.properties.ComponentProperties;
 import org.talend.core.model.components.IComponent;
 import org.talend.core.model.components.IMultipleComponentManager;
@@ -89,8 +88,6 @@ public abstract class AbstractNode implements INode {
     private EConnectionType virtualLinkTo;
 
     private String uniqueShortName;
-
-    private String generatingUniqueName;
 
     private boolean subProcessContainBreakpoint;
 
@@ -1342,18 +1339,5 @@ public abstract class AbstractNode implements INode {
 
     public void setRealGraphicalNode(INode realGraphicalNode) {
         this.realGraphicalNode = realGraphicalNode;
-    }
-
-    @Override
-    public String getGeneratingUniqueName() {
-        if (StringUtils.isNotBlank(generatingUniqueName)) {
-            return generatingUniqueName;
-        }
-        return getUniqueName();
-    }
-
-    @Override
-    public void setGeneratingUniqueName(String generatingUniqueName) {
-        this.generatingUniqueName = generatingUniqueName;
     }
 }
