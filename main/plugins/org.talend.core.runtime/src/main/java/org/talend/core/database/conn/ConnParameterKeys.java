33162c8be4a3ea88eaa3f715bddc0a7824d1e716
sponomarova <42870019+sponomarova@users.noreply.github.com>
2023-05-05 15:45:59 +0300

    feat(TBD-15065): Synapse with universal : GA (#6149)

--- main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/database/conn/ConnParameterKeys.java
+++ main/plugins/org.talend.core.runtime/src/main/java/org/talend/core/database/conn/ConnParameterKeys.java
@@ -252,6 +252,8 @@ public class ConnParameterKeys {
     
     public static final String CONN_PARA_KEY_EXECUTOR_MEMORY = "CONN_PARA_KEY_EXECUTOR_MEMORY"; //$NON-NLS-1$
     
+    public static final String CONN_PARA_KEY_EXECUTOR_CORES = "CONN_PARA_KEY_EXECUTOR_CORES";
+    
     public static final String CONN_PARA_KEY_TUNING_PROPERTIES = "CONN_PARA_KEY_TUNING_PROPERTIES"; //$NON-NLS-1$
 
     /******************************************/
