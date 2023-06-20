2e64107d545780ae16806cb3992a39f7b03dfec4
kjwang-talend <kjwang@talend.com>
2023-06-20 10:27:58 +0800

    Improve the code

--- main/plugins/org.talend.model/src/main/java/org/talend/core/model/metadata/builder/connection/TacokitDatabaseConnection.java
+++ main/plugins/org.talend.model/src/main/java/org/talend/core/model/metadata/builder/connection/TacokitDatabaseConnection.java
@@ -13,9 +13,8 @@
 package org.talend.core.model.metadata.builder.connection;
 
 /**
- * <!-- begin-user-doc -->
- * A representation of the model object '<em><b>Tacokit Database Connection</b></em>'.
- * <!-- end-user-doc -->
+ * <!-- begin-user-doc --> A representation of the model object '<em><b>Tacokit Database Connection</b></em>'. <!--
+ * end-user-doc -->
  *
  *
  * @see org.talend.core.model.metadata.builder.connection.ConnectionPackage#getTacokitDatabaseConnection()
@@ -23,97 +22,99 @@ package org.talend.core.model.metadata.builder.connection;
  * @generated
  */
 public interface TacokitDatabaseConnection extends DatabaseConnection {
-    
+
     String KEY_DRIVER = "configuration.jdbcDriver";
-    
+
     String KEY_DRIVER_CLASS = "configuration.jdbcClass";
-    
+
     String KEY_URL = "configuration.jdbcUrl";
-    
+
     String KEY_PORT = "configuration.port";
-    
+
     String KEY_HOST = "configuration.host";
-    
+
     String KEY_USER_ID = "configuration.userId";
-    
+
     String KEY_PASSWORD = "configuration.password";
-    
+
     String KEY_DATABASE_MAPPING = "configuration.dbMapping";
-    
+
     String KEY_USE_SHARED_DB_CONNECTION = "configuration.useSharedDBConnection";
-    
+
     String KEY_SHARED_DB_CONNECTION = "configuration.sharedDBConnectionName";
-    
+
     String KEY_USE_DATASOURCE = "configuration.useDataSource";
-    
-    String KEY_DATASOURCE_ALIAS = "configuration.dataSourceAlias"; 
-    
+
+    String KEY_DATASOURCE_ALIAS = "configuration.dataSourceAlias";
+
     String KEY_AUTHENTICATION_TYPE = "configuration.authenticationType";
-    
+
     String KEY_USE_AUTO_COMMIT = "configuration.useAutoCommit";
-    
+
     String KEY_AUTO_COMMIT = "configuration.autoCommit";
-    
+
     String KEY_ENABLE_DB_TYPE = "configuration.enableDBType";
-    
+
     String KEY_DB_TYPE = "configuration.dbType";
-    
+
     String KEY_DATASTORE_DRIVER = "configuration.dataSet.dataStore.jdbcDriver";
-    
+
     String KEY_DATASTORE_DRIVER_CLASS = "configuration.dataSet.dataStore.jdbcClass";
-    
+
     String KEY_DATASTORE_URL = "configuration.dataSet.dataStore.jdbcUrl";
-    
+
     String KEY_DATASTORE_PORT = "configuration.dataSet.dataStore.port";
-    
+
     String KEY_DATASTORE_HOST = "configuration.dataSet.dataStore.host";
-    
+
     String KEY_DATASTORE_USER_ID = "configuration.dataSet.dataStore.userId";
-    
+
     String KEY_DATASTORE_PASSWORD = "configuration.dataSet.dataStore.password";
-    
+
     String KEY_DATASTORE_DATABASE_MAPPING = "configuration.dataSet.dataStore.dbMapping";
-    
+
     String KEY_DATASTORE_USE_SHARED_DB_CONNECTION = "configuration.dataSet.dataStore.useSharedDBConnection";
-    
+
     String KEY_DATASTORE_SHARED_DB_CONNECTION = "configuration.dataSet.dataStore.sharedDBConnectionName";
-    
+
     String KEY_DATASTORE_USE_DATASOURCE = "configuration.dataSet.dataStore.useDataSource";
-    
-    String KEY_DATASTORE_DATASOURCE_ALIAS = "configuration.dataSet.dataStore.dataSourceAlias"; 
-    
+
+    String KEY_DATASTORE_DATASOURCE_ALIAS = "configuration.dataSet.dataStore.dataSourceAlias";
+
     String KEY_DATASTORE_AUTHENTICATION_TYPE = "configuration.dataSet.dataStore.authenticationType";
-    
+
     String KEY_DATASTORE_USE_AUTO_COMMIT = "configuration.dataSet.dataStore.useAutoCommit";
-    
+
     String KEY_DATASTORE_AUTO_COMMIT = "configuration.dataSet.dataStore.autoCommit";
-    
+
     String KEY_DATASTORE_ENABLE_DB_TYPE = "configuration.dataSet.dataStore.enableDBType";
-    
+
     String KEY_DATASTORE_DB_TYPE = "configuration.dataSet.dataStore.dbType";
-    
-    String KEY_DATASET_SQL_QUERY="configuration.dataSet.sqlQuery";
-    
-    String KEY_DATASET_TABLE_NAME="configuration.dataSet.tableName";
-      
+
+    String KEY_DATASET_SQL_QUERY = "configuration.dataSet.sqlQuery";
+
+    String KEY_DATASET_TABLE_NAME = "configuration.dataSet.tableName";
+
+    String KEY_JDBC_DATASTORE_NAME = "JDBCDataStore";
+
     public String getDatabaseMappingFile();
-    
+
     public boolean useSharedDBConnection();
-    
+
     public String getSharedDBConnectionName();
-    
+
     public boolean useDatasourceAlias();
-    
+
     public String getDatasourceAlias();
-    
+
     public String getAuthenticationType();
-    
+
     public boolean useAutoCommit();
-    
+
     public boolean autoCommit();
 
     public boolean enableDBType();
-    
+
     public Object getPropertyValue(String key);
-    
+
 } // TacokitDatabaseConnection
