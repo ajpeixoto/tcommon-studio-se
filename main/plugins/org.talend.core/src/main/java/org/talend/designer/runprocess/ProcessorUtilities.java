a2ce928d6ac4d9a70b2b0619c91731c3d3febb15
apoltavtsev <apoltavtsev@gmail.com>
2023-09-11 10:35:07 +0200

    APPINT-35888: Avoid "duplicated in the reactor" for Jobs with Routines

--- main/plugins/org.talend.core/src/main/java/org/talend/designer/runprocess/ProcessorUtilities.java
+++ main/plugins/org.talend.core/src/main/java/org/talend/designer/runprocess/ProcessorUtilities.java
@@ -1633,7 +1633,7 @@ public class ProcessorUtilities {
                                     }
                                 }
                                 
-                                if (CommonsPlugin.isHeadless() && interpreter != null) {
+                                if (("cTalendJob".equals(componentName) || "Routelets".equals(node.getComponent().getOriginalFamilyName())) && CommonsPlugin.isHeadless() && interpreter != null) {
                                 	processItem.getProperty().setParentItem(ItemCacheManager.getProcessItem(currentProcess.getId(), currentProcess.getVersion()));
                                 }
 
