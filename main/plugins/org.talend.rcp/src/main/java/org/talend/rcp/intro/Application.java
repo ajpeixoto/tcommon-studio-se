6d8b1b51c7f7fc2f0de2498e66dd4e2228c6ba46
bhe-talendbj <52311292+bhe-talendbj@users.noreply.github.com>
2024-01-03 08:46:10 +0000

    fix(TUP-41391): initialized workspace location too early (#6690)

--- main/plugins/org.talend.rcp/src/main/java/org/talend/rcp/intro/Application.java
+++ main/plugins/org.talend.rcp/src/main/java/org/talend/rcp/intro/Application.java
@@ -51,7 +51,6 @@ import org.talend.commons.runtime.service.ComponentsInstallComponent;
 import org.talend.commons.runtime.service.PatchComponent;
 import org.talend.commons.ui.runtime.update.PreferenceKeys;
 import org.talend.commons.ui.swt.dialogs.ErrorDialogWidthDetailArea;
-import org.talend.commons.utils.generation.JavaUtils;
 import org.talend.commons.utils.network.NetworkUtil;
 import org.talend.commons.utils.network.TalendProxySelector;
 import org.talend.commons.utils.system.EclipseCommandLine;
@@ -116,7 +115,7 @@ public class Application implements IApplication {
         Display display = PlatformUI.createDisplay();
 
         try {
-            JavaUtils.validateJavaVersion();
+            StudioKeysFileCheck.validateJavaVersion();
         } catch (Exception e) {
             Shell shell = new Shell(display, SWT.NONE);
             if (e instanceof VersionException) {
